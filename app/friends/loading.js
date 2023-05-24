e15c85e4daa77045807e7aea898dcfcd455fc1b9
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2023-05-23 22:05:56 -0400

    adjust loading component

--- app/friends/loading.js
+++ app/friends/loading.js
@@ -2,5 +2,13 @@ import PlaceHolder from '../../public/contact_placeholder.svg'
 import styles from './loading.module.css'
 
 export default function Loading() {
-    return <PlaceHolder className={styles.container}/>
+    return (
+        <div className={styles.container}>
+            {Array(9).fill('unused').map((el, idx) => (
+                    // acceptable to use the id since these components will never change order
+                    <PlaceHolder key={idx} className={styles.loader}/>
+            ))}
+        </div>
+        
+    )
 }
--- app/friends/page.js
+++ app/friends/page.js
@@ -2,6 +2,15 @@ import styles from './page.module.css'
 import FriendsList from '@/components/friends/FriendsList'
 import ServerFriendsList from '@/components/friends/ServerFriendsList'
 
+// uncomment to test loading screen and make Friends await for it
+/*
+const pro = () => new Promise((res, rej) => {
+    setTimeout(()=>{
+        res()
+    }, 3000);
+})
+*/
+
 export default function Friends() {
     return (
         <div className={styles.container}>
